b111e59999b86eb561d2921ad3b356d5615d69d2
Sydeilim <martiles@fi.uba.ar>
2026-06-09 17:09:51 +0000

    Fix compilacion

--- server/src/responses/response_map.cpp
+++ server/src/responses/response_map.cpp
@@ -1,6 +1,6 @@
 #include "server/includes/responses/response_map.h"
 
 #include "server/includes/server_protocol.h"
-ResponseMap::ResponseMap(const Map& map): map(map) {}
+ResponseMap::ResponseMap(Map&& map): map(std::move(map)) {}
 
 void ResponseMap::execute(ServerProtocol& protocol) { protocol.sendMap(this->map); }
